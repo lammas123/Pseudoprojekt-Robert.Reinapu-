2dffdb720d26fc1d8fb6195df6bd5333b700015d
Robert Reinapu <robert.reinapu@khk.ee>
2015-09-23 11:51:02 +0300

    2. Tere maailm button [Deliver #103976740]

--- Kliendipoolsed.php
+++ Kliendipoolsed.php
@@ -5,6 +5,18 @@
     <title>Document</title>
 </head>
 <body>
-<p>Hi</p>
+<button onclick="myFunction()"><a href="https://khk.ee">Tere maailm</button>
+<script>
+    function myFunction() {
+        var x;
+        if (confirm("Tere maailm!") == true) {
+            x = "You pressed OK!";
+        } else {
+            x = "You pressed Cancel!";
+        }
+        document.getElementById("demo").innerHTML = x;
+    }
+</script>
+
 </body>
 </html>
