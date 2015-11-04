8ab157393e226d8b3dfdbe51954caa3a7431abee
Robert Reinapu <robert.reinapu@khk.ee>
2015-11-04 12:44:30 +0200

    13. Stringid [Deliver #103976764]

---
+++ serveripoolsed.php
@@ -0,0 +1,5 @@
+<?php
+$firstname = "Robert";
+$lastname = "Reinapu";
+
+echo $firstname . " " . $lastname;
--- tere.php
+++ tere.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: DataKeyt
- * Date: 17.10.2015
- * Time: 13:37
- */
 echo "Tere maailm";
