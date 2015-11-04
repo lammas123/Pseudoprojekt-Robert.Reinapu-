d104d0db95a21673aeb70155fec7ad96e1a4a891
Robert Reinapu <robert.reinapu@khk.ee>
2015-11-04 13:03:29 +0200

    17. Tingimused [Deliver #103976772]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -10,5 +10,10 @@ $person = array(
 //boolean
 $eesnimi_algab_vokaaliga = false;
 
+//Tingimused
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
+
 echo "<p>" . $person['firstname'] . " " . $person['lastname'] . " (" . $person['age'] . ")</p>";
 
