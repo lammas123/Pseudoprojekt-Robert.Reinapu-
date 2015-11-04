3b1a223d5c68182eb4b17124e8d53e20e6c7e9f8
Robert Reinapu <robert.reinapu@khk.ee>
2015-11-04 12:53:13 +0200

    15. Boolean [Deliver #103976768]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,6 +1,12 @@
 <?php
+//strings
 $firstname = "Robert";
 $lastname = "Reinapu";
+
+//Integer
 $age = 19;
+
+//boolean
+$eesnimi_algab_vokaaliga = false;
 
 echo $firstname . " " . $lastname . " ($age)";
