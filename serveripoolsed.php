dd97b51ce6535efe593c422ecdc7366fd9c9cb65
Robert Reinapu <robert.reinapu@khk.ee>
2015-11-04 13:10:04 +0200

    19. Funktsioonid [Deliver #103976778]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,20 +1,5 @@
 <?php
 
-//Massiiv
-/*$person = array(
-    "firstname" => "Robert",
-    "lastname" => "Reinapu",
-    "age" => "19"
-
-); */
-
-//boolean
-$eesnimi_algab_vokaaliga = false;
-
-//Tingimused
-if ($eesnimi_algab_vokaaliga) {
-    echo "<p>Nimi algab vokaaliga</p>";
-};
 
 class isik
 {
@@ -25,6 +10,20 @@ class isik
 }
 
 $Robert = new isik;
+
+//Funktsioon
+$vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
+if (in_array(substr($Robert->firstname, 0, 1), $vowels)) {
+    $eesnimi_algab_vokaaliga = true;
+} else {
+    $eesnimi_algab_vokaaliga = false;
+}
+
+
+//Tingimused
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
 
 
 echo "<p>" . $Robert->firstname . " " . $Robert->lastname . " (" . $Robert->age . ") " . $Robert->sex . "</p>";
